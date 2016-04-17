1dc8c937288d3492f7a57478ad100930f6e97812
Sean Aubin <saubin@uwaterloo.ca>
2016-04-17 19:53:20 -0400

    svm draft 2

--- svm_nengo_test.py
+++ svm_nengo_test.py
@@ -23,6 +23,12 @@ def main(t_len, dims, n_classes, dataset, testset):
 
     n_neurons = 200
     tau = 0.1
+    # setup the feature craziness
+    freq_range = (0, 500)
+    Q_range = (2, 50)
+    features_per_dim = 400
+    feat_pops = []
+    feat_list = []
 
     # make a model and run it to get spiking data
     # as acquired when passed through multiple bandpass filters
@@ -35,18 +41,13 @@ def main(t_len, dims, n_classes, dataset, testset):
 
         nengo.Connection(feed_net.q_in, state.input, synapse=None)
 
-        # setup the feature craziness
-        freq_range = (0, 500)
-        Q_range = (2, 50)
-        features_per_dim = 400
-        feat_pops = []
-        feat_list = []
 
         for dim in range(dims):
             # this declaration needed for the multisynapse transform
             encoders = nengo.dists.UniformHypersphere(
                         surface=True).sample(features_per_dim, 1)
-            feat_pop = nengo.Ensemble(features_per_dim, 1, encoders=encoders)
+            feat_pop = nengo.Ensemble(features_per_dim, 1, encoders=encoders,
+                seed=SEED+dim)
             feat_pops.append(feat_pop)
 
             sub_features = zip(
@@ -85,27 +86,45 @@ def main(t_len, dims, n_classes, dataset, testset):
     ipdb.set_trace()
     clf = svm.LinearSVC().fit(X, Y)
     # Need to use clf.classes_, clf.coef_, clf.intercept_
-    print("rmse: %s" %info["rmse"])
+
+    # this basically just forms the coefficients into weights, it's basically a reshape operation
+    weights = np.zeros((len(feature_list), features_per_dim, n_classes))
+    for i in feature_sliced:
+        weights[
+            i // features_per_dim, i % features_per_dim] = clf.coef_[:, i]
+    intercept = clf.intercept_
 
     # run the test data with the SVM
-
-    test_model = nengo.Network()
     with test_model:
-        feed_net = create_feed_net(testset[0], testset[1], t_len, dims, n_classes)
-        output = nengo.Node(size_in=n_classes, size_out=n_classes)
-        normal = nengo.Node(size_in=dims, size_out=dims)
+        # this may not work
+        feed_net.d_f.__init__(testset[0], testset[1], t_len, dims, n_classes)
 
-        state = nengo.Ensemble(n_neurons=n_neurons, dimensions=dims,
-                               radius=np.sqrt(2), seed=SEED)
-        nengo.Connection(state.neurons, state.neurons,
-                         transform=weights / n_neurons, synapse=tau)
+        # synapses for smoothing, because that's important according to Aaron's paper
+        predict_tau = 0.005
+        lowpass = 0.05
+        tau_ratio = predic_tau / lowpass
 
-        nengo.Connection(feed_net.q_in, state, synapse=None)
-        nengo.Connection(state.neurons, output, transform=decoders.T)
-        nengo.Connection(output, feed_net.set_ans, synapse=None)
-        nengo.Connection(state, normal)
+        scores = nengo.Node(size_in=n_classes)
 
-        p_out = nengo.Probe(output)
+        predict = nengo.networks.EnsembleArray(n_neurons, n_classes, seed=SEED+1)
+        nengo.Connection(scores, predict.input, transform=tau_ratio,
+            synapse=predict_tau)
+        nengo.Connection(predict.input, predict.output,
+            transform=1-tau_ratio, synapse=predict_tau)
+
+        for dim in range(dims):
+            w = weights[dim]
+            subset = (w != 0).any(axis=1)
+            assert subset.shape == (features_per_dim,)
+
+            nengo.Connection(feat_pops[dim].neurons, scores,
+                transform=w[subset].T, synapse=None)
+
+        bias = nengo.Node(output=[1], label="bias")
+        nengo.Connection(bias, scores,
+                 transform=intercept[:, None], synapse=None)
+
+        p_out = nengo.Probe(predict.output)
         p_correct = nengo.Probe(feed_net.get_ans)
 
     print("test simulation start")
