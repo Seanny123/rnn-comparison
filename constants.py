78f135b98d34f824fdc2e3095c1800cbc3fec3e7
Sean Aubin <saubin@uwaterloo.ca>
2016-05-19 14:43:49 -0400

    remove files

--- constants.py
+++
@@ -1,7 +0,0 @@
-# random number seed
-SEED = 0
-# simulator time step
-dt = 0.001
-# amount to pause between questions
-PAUSE = 0.01
-
--- gru_test.py
+++
@@ -1,7 +0,0 @@
-import lasagne
-import scipy.io
-
-def main(t_len, dims, n_classes, val_func, prob_type):
-    """given dimensions, t_len, """
-
-    # if not discrete, discretize input signal
